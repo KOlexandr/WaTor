db7a4dbb80928248c2931579f4a86f52245af27e
Olexandr <kolexandr.13@gmail.com>
2014-10-25 00:39:19 +0300

    Charts

--- src/main/java/WaTor.java
+++ src/main/java/WaTor.java
@@ -3,10 +3,22 @@ import scala.Tuple3;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.*;
 import java.util.List;
 
 class WaTorCanvas extends Canvas {
+
+    public static final int DEFAULT_WIDTH = 40;
+    public static final int DEFAULT_HEIGHT = 40;
+    public static final int DEFAULT_TIMEOUT = 100;
+    public static final int DEFAULT_FISH_COUNT = 50;
+    public static final int DEFAULT_SHARK_COUNT = 5;
+    public static final int DEFAULT_SHARK_ENERGY = 20;
+    public static final int DEFAULT_FISH_REPR_TIME = 8;
+    public static final int DEFAULT_SHARK_REPR_TIME = 15;
+
     private int step = 0;
     private World world;
     private Graphics graphics;
@@ -21,6 +33,7 @@ class WaTorCanvas extends Canvas {
     private int sharkEnergy;
 
     private boolean running = false;
+    private int timeout = DEFAULT_TIMEOUT;
 
     private java.util.List<Integer> sharks = new ArrayList<>();
     private java.util.List<Integer> fishes = new ArrayList<>();
@@ -38,10 +51,37 @@ class WaTorCanvas extends Canvas {
         fishes.add(world.countFishes());
     }
 
+    public WaTorCanvas(){
+        this(
+            DEFAULT_WIDTH,
+            DEFAULT_HEIGHT,
+            DEFAULT_FISH_COUNT,
+            DEFAULT_FISH_REPR_TIME,
+            DEFAULT_SHARK_COUNT,
+            DEFAULT_SHARK_REPR_TIME,
+            DEFAULT_SHARK_ENERGY
+        );
+    }
+
     public void reload(int width, int height, int fishCount, int fishReprTime, int sharkCount, int sharkReprTime, int sharkEnergy){
+        fishes.clear();
+        sharks.clear();
         this.width = width;
         this.height = height;
         world = new World(new Tuple2<>(height, width), new Tuple2<>(fishCount, fishReprTime), new Tuple3<>(sharkCount, sharkReprTime, sharkEnergy));
+    }
+
+    public void reload(){
+        sharks.clear();
+        fishes.clear();
+        this.width = DEFAULT_WIDTH;
+        this.height = DEFAULT_HEIGHT;
+        this.timeout = DEFAULT_TIMEOUT;
+        world = new World(
+                new Tuple2<>(DEFAULT_WIDTH, DEFAULT_HEIGHT),
+                new Tuple2<>(DEFAULT_FISH_COUNT, DEFAULT_FISH_REPR_TIME),
+                new Tuple3<>(DEFAULT_SHARK_COUNT, DEFAULT_SHARK_REPR_TIME, DEFAULT_SHARK_ENERGY)
+        );
     }
 
     public void setRunning(boolean r) {
@@ -70,7 +110,7 @@ class WaTorCanvas extends Canvas {
             return;
         }
         try {
-            Thread.sleep(100);
+            Thread.sleep(timeout);
         } catch (InterruptedException ignored){}
         world.forward(step++);
         sharks.add(world.countSharks());
@@ -120,6 +160,19 @@ class WaTorCanvas extends Canvas {
         return sharkEnergy;
     }
 
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public String setTimeout(String timeout) {
+        try{
+            this.timeout = Integer.valueOf(timeout);
+        } catch (Exception e){
+            this.timeout = DEFAULT_TIMEOUT;
+        }
+        return String.valueOf(this.timeout);
+    }
+
     public List<Integer> getSharks() {
         return sharks;
     }
@@ -158,6 +211,7 @@ class ScrollField extends Panel {
 
 public class WaTor extends Applet {
     private WaTorCanvas wc;
+    private TextField timeoutField;
     private TextField initFishField;
     private TextField initSharkField;
     private ScrollField fishBreedScrollField;
@@ -166,16 +220,19 @@ public class WaTor extends Applet {
 
     public void init() {
         setLayout(new FlowLayout());
-        setSize(800, 400);
+        setSize(1000, 450);
 
-        wc = new WaTorCanvas(40, 40, 50, 8, 5, 15, 20);
+        wc = new WaTorCanvas();
         wc.setSize(400, 400);
         add(wc);
 
         final Panel p = new Panel();
-        p.setLayout(new GridLayout(8, 1, 3, 3));
+        p.setLayout(new GridLayout(11, 1, 3, 3));
         final Button start = new Button("Start");
-        start.addActionListener(e -> wc.setRunning(true));
+        start.addActionListener(e -> {
+            wc.setRunning(true);
+            timeoutField.setText(wc.setTimeout(timeoutField.getText()));
+        });
         p.add(start);
         final Button stop = new Button("Stop");
         stop.addActionListener(e -> wc.setRunning(false));
@@ -183,26 +240,28 @@ public class WaTor extends Applet {
         final Button reset = new Button("Reset");
         reset.addActionListener(e -> {
             wc.setRunning(false);
-            initFishField.setText(String.valueOf(50));
-            fishBreedScrollField.setValue(8);
-            initSharkField.setText(String.valueOf(5));
-            sharkBreedScrollField.setValue(15);
-            sharkStarveScrollField.setValue(20);
-            wc.reload(40, 40, 50, 8, 5, 15, 20);
+            wc.reload();
+            initFishField.setText(String.valueOf(wc.getFishCount()));
+            fishBreedScrollField.setValue(wc.getFishReprTime());
+            initSharkField.setText(String.valueOf(wc.getSharkCount()));
+            sharkBreedScrollField.setValue(wc.getSharkReprTime());
+            sharkStarveScrollField.setValue(wc.getSharkEnergy());
+            timeoutField.setText(String.valueOf(wc.getTimeout()));
         });
         p.add(reset);
         final Button applyNewSettings = new Button("Apply new Settings");
         applyNewSettings.addActionListener(e -> {
             wc.setRunning(false);
             wc.reload(
-                    40,
-                    40,
+                    WaTorCanvas.DEFAULT_WIDTH,
+                    WaTorCanvas.DEFAULT_HEIGHT,
                     new Integer(initFishField.getText()),
                     fishBreedScrollField.getValue(),
                     new Integer(initSharkField.getText()),
                     sharkBreedScrollField.getValue(),
                     sharkStarveScrollField.getValue()
             );
+            timeoutField.setText(wc.setTimeout(timeoutField.getText()));
         });
         p.add(applyNewSettings);
 
@@ -218,6 +277,20 @@ public class WaTor extends Applet {
         shark.add(initSharkField);
         p.add(shark);
 
+        final Panel timeout = new Panel();
+        timeout.add(new Label("Repaint Timeout"));
+        timeoutField = new TextField(Integer.toString(wc.getTimeout()), 5);
+        timeout.add(timeoutField);
+        timeoutField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    timeoutField.setText(wc.setTimeout(timeoutField.getText()));
+                }
+            }
+        });
+        p.add(timeout);
+
         fishBreedScrollField = new ScrollField("Fish Breed Age", wc.getFishReprTime());
         p.add(fishBreedScrollField);
 
@@ -227,9 +300,5 @@ public class WaTor extends Applet {
         sharkStarveScrollField = new ScrollField("Shark Starve Time", wc.getSharkEnergy());
         p.add(sharkStarveScrollField);
         add(p);
-
-//        final Button test = new Button("Test");
-//        test.addActionListener(e -> wc.plotSharlFish());
-//        add(test);
     }
 }
